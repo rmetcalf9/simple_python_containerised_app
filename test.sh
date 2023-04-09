acda55f5ed2555ade2ef65251241f6c1e960dc1b
Robert Metcalf <rmetcalf9@googlemail.com>
2023-04-09 10:29:45 +0100

    Bug
    use equals to set param

--- test.sh
+++ test.sh
@@ -11,7 +11,7 @@ source ./_repo_vars.sh
 echo "Testing project ${PROJECT_NAME}"
 
 docker run --rm -it \
-  -e "PYTHONPYCACHEPREFIX:/tmp/__python_cache_dir__" \
+  -e "PYTHONPYCACHEPREFIX=/tmp/__python_cache_dir__" \
   -v $(pwd):/maindir \
   -w /maindir \
   ${BUILD_IMAGE_NAME_AND_TAG} python3 -m pytest -p no:cacheprovider
